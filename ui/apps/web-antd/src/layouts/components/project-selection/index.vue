c9b6100226703bef83978db2687f0260f214b99f
chenqi99 <chenqi99@nancal.com>
2024-12-21 13:10:32 +0800

    update

--- ui/apps/web-antd/src/layouts/components/project-selection/index.vue
+++ ui/apps/web-antd/src/layouts/components/project-selection/index.vue
@@ -13,14 +13,16 @@ function selectProject(item) {
     <template #overlay>
       <Menu @click="selectProject">
         <MenuItem key="1" @click="selectProject(1)">
-          <span class="dp-dropdown-icon icon-[ant-design--project-outlined]" />
-          &nbsp;项目B
+          <span
+            class="dp-dropdown-icon icon-[ant-design--project-outlined]"
+          ></span>
+          <span class="dp-inline-block" style="padding-left: 10px">项目B</span>
         </MenuItem>
       </Menu>
     </template>
 
     <span>
-      <span>项目A</span>
+      <span class="dp-inline-block" style="padding-right: 3px">项目A</span>
       <span class="dp-dropdown-icon icon-[ant-design--down-outlined]"></span>
     </span>
   </Dropdown>
