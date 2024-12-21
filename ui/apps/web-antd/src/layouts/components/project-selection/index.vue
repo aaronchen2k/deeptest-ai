f6aa1ce152544ea5ec182dc5188d33bbade8e2c8
chenqi99 <chenqi99@nancal.com>
2024-12-21 12:35:23 +0800

    update

--- ui/apps/web-antd/src/layouts/components/project-selection/index.vue
+++ ui/apps/web-antd/src/layouts/components/project-selection/index.vue
@@ -1,47 +1,27 @@
 <script lang="ts" setup>
 import '@vben/styles/global';
 
-import { Dropdown, Menu, MenuItem } from 'ant-design-vue';
+import { Dropdown, Menu, MenuItem, Button } from 'ant-design-vue';
 
-function selectProject() {
-  window.console.log('selectProject');
+function selectProject(item) {
+  window.console.log('selectProject', item);
 }
 </script>
 
 <template>
-  <Dropdown>
+  <Dropdown class="dp-dropdown" overlay-class-name="dp-dropdown-overlay">
     <template #overlay>
       <Menu @click="selectProject">
-        <MenuItem key="1">
-          <span
-            class="icon-[ant-design--project-outlined]"
-            style="mask-position: 0 3px"
-          ></span>
-          &nbsp;项目A
-        </MenuItem>
-        <MenuItem key="2">
-          <span
-            class="icon-[ant-design--project-outlined]"
-            style="mask-position: 0 3px"
-          ></span>
+        <MenuItem key="1" @click="selectProject(1)">
+          <span class="item-icon icon-[ant-design--project-outlined]" />
           &nbsp;项目B
-        </MenuItem>
-        <MenuItem key="3">
-          <span
-            class="icon-[ant-design--project-outlined]"
-            style="mask-position: 0 3px"
-          ></span>
-          &nbsp;项目C
         </MenuItem>
       </Menu>
     </template>
 
-    <a-button class="dp-link">
-      <span>项目A</span>&nbsp;
-      <span
-        class="icon-[ant-design--down-outlined]"
-        style="mask-position: 2px 3px; font-weight: bold"
-      ></span>
-    </a-button>
+    <span>
+      <span>项目A</span>
+      <span class="item-icon icon-[ant-design--down-outlined]"></span>
+    </span>
   </Dropdown>
 </template>
